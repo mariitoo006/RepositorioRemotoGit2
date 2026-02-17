e73dbd5dbc559a692017010a94d9929386767da2
FusionRamas <Mario@DESKTOP-GRU9AR0>
2026-02-17 16:57:48 +0100

    Tercer Commit

--- Proyecto2/src/Ejemplo/Principal.java
+++ Proyecto2/src/Ejemplo/Principal.java
@@ -4,6 +4,9 @@ public class Principal {
 	public static void main (String [] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola Mundo 2");
+		
+		System.out.println("Hola Mundo 3");
+		System.out.println("Hola Mundo 4");
 	}
 
 }
